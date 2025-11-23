b34a9bf1fe5d52d2889ae69239822fd22a7f02f4
rtae <natthanan.bhu@gmail.com>
2025-11-23 07:50:07 +0000

    Update MPPI configuration; increase number of parallel workers for improved performance.

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
     pop_size: int = 64             # total MPPI samples per iteration
     es_sigma: float = 0.06         # exploration noise
     mppi_lambda: float = 4.0       # smoother weighting, much more stable
-    cem_workers: int = 8           # parallel workers
+    cem_workers: int = 22           # parallel workers
     es_batch:int = 8               # number of samples per batch
     w_track = 0.02
     w_task = 1.0
