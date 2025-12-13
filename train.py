9cbcaa35e7f8af5f5fe49e6466d4e03a3f56bc4f
rtae <natthanan.bhu@gmail.com>
2025-12-13 02:01:32 +0000

    add callback eval

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from env_factory import build_env
 from utils import make_predict_fn, prepare_experiment_directory
 from callbacks.video_callback import VideoCallback
 from callbacks.infologger_callback import InfoLoggerCallback
+from stable_baselines3.common.callbacks import EvalCallback
 
 
 def main():
@@ -14,6 +15,7 @@ def main():
     prepare_experiment_directory(cfg)
 
     env = build_env(cfg)
+    eval_env = build_env(cfg)
 
     model = PPO(
         "MlpPolicy",
@@ -32,11 +34,18 @@ def main():
 
     video_cb = VideoCallback(cfg, predict_fn=make_predict_fn(model))
     info_cb = InfoLoggerCallback(prefix="train/info")
+    eval_callback = EvalCallback(
+                    eval_env,
+                    best_model_save_path=cfg.logdir,
+                    log_path=os.path.join(cfg.logdir, "eval"),
+                    eval_freq=cfg.eval_freq,
+                    deterministic=True, render=False)
 
     model.learn(
         total_timesteps=cfg.total_timesteps,
-        callback=CallbackList([video_cb, info_cb]),
+        callback=CallbackList([video_cb, info_cb, eval_callback]),
     )
+    
 
     model.save(os.path.join(cfg.logdir, "model"))
     env.close()
