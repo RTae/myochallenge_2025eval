d4cc1c3c80b3927811c3693c9de0af49eb68ced6
rtae <natthanan.bhu@gmail.com>
2025-12-14 04:02:53 +0000

    refactor: comment out unused training and evaluation code in main function

--- train.py
+++ train.py
@@ -15,66 +15,67 @@ from worker_env import TableTennisWorker
 
 def main():
     cfg = Config()
-    prepare_experiment_directory(cfg)
+    #prepare_experiment_directory(cfg)
+    cfg.logdir = "./logs/exp15"
 
-    # Train worker
+    # # Train worker
     worker_cfg = copy.deepcopy(cfg)
-    worker_cfg.logdir = os.path.join(cfg.logdir, "worker")
-    env_worker = build_env(worker_cfg, worker=True)
+    # worker_cfg.logdir = os.path.join(cfg.logdir, "worker")
+    # env_worker = build_env(worker_cfg, worker=True)
 
-    worker_model = RecurrentPPO(
-        policy=LatticeRecurrentActorCriticPolicy,
-        env=env_worker,
-        tensorboard_log=worker_cfg.logdir,
-        verbose=1,
-        device="auto",
-        batch_size=worker_cfg.ppo_batch_size,
-        n_steps=worker_cfg.ppo_n_steps,
-        n_epochs=worker_cfg.ppo_epochs,
-        learning_rate=worker_cfg.ppo_lr,
-        clip_range=worker_cfg.ppo_clip,
-        gamma=worker_cfg.ppo_gamma,
-        gae_lambda=worker_cfg.ppo_lambda,
-        ent_coef=3.62109e-06,
-        max_grad_norm=0.7,
-        vf_coef=0.835671,
-        policy_kwargs=dict(
-            use_lattice=True,
-            use_expln=True,
-            ortho_init=False,
-            log_std_init=0.0,
-            std_clip=(1e-3, 10),
-        ),
-    )
+    # worker_model = RecurrentPPO(
+    #     policy=LatticeRecurrentActorCriticPolicy,
+    #     env=env_worker,
+    #     tensorboard_log=worker_cfg.logdir,
+    #     verbose=1,
+    #     device="auto",
+    #     batch_size=worker_cfg.ppo_batch_size,
+    #     n_steps=worker_cfg.ppo_n_steps,
+    #     n_epochs=worker_cfg.ppo_epochs,
+    #     learning_rate=worker_cfg.ppo_lr,
+    #     clip_range=worker_cfg.ppo_clip,
+    #     gamma=worker_cfg.ppo_gamma,
+    #     gae_lambda=worker_cfg.ppo_lambda,
+    #     ent_coef=3.62109e-06,
+    #     max_grad_norm=0.7,
+    #     vf_coef=0.835671,
+    #     policy_kwargs=dict(
+    #         use_lattice=True,
+    #         use_expln=True,
+    #         ortho_init=False,
+    #         log_std_init=0.0,
+    #         std_clip=(1e-3, 10),
+    #     ),
+    # )
 
-    # Callback Share
+    # # Callback Share
     info_cb = InfoLoggerCallback(prefix="train/info")
     eval_cfg = Config()
     eval_cfg.num_envs = 1
     
-    # Callback Worker
-    eval_worker_env = build_env(eval_cfg, worker=True)
-    eval_worker_cb = EvalCallback(
-        eval_worker_env,
-        best_model_save_path=os.path.join(worker_cfg.logdir, "best_model"),
-        log_path=os.path.join(worker_cfg.logdir, "eval"),
-        eval_freq=worker_cfg.eval_freq,
-        n_eval_episodes=worker_cfg.eval_episodes,
-        deterministic=True,
-        render=False,
-    )
-    video_cb = VideoCallback(worker_cfg, make_predict_fn(worker_model))
+    # # Callback Worker
+    # eval_worker_env = build_env(eval_cfg, worker=True)
+    # eval_cb = EvalCallback(
+    #     eval_worker_env,
+    #     best_model_save_path=os.path.join(worker_cfg.logdir, "best_model"),
+    #     log_path=os.path.join(worker_cfg.logdir, "eval"),
+    #     eval_freq=worker_cfg.eval_freq,
+    #     n_eval_episodes=worker_cfg.eval_episodes,
+    #     deterministic=True,
+    #     render=False,
+    # )
+    # video_cb = VideoCallback(worker_cfg, make_predict_fn(worker_model))
 
-    # Learn
-    worker_model.learn(
-        total_timesteps=worker_cfg.total_timesteps,
-        callback=CallbackList([info_cb, eval_worker_cb, video_cb]),
-    )
+    # # Learn
+    # worker_model.learn(
+    #     total_timesteps=worker_cfg.total_timesteps,
+    #     callback=CallbackList([info_cb, eval_cb, video_cb]),
+    # )
 
     worker_model_path = os.path.join(worker_cfg.logdir, "model.pkl")
-    worker_model.save(worker_model_path)
-    eval_worker_env.close()
-    env_worker.close()
+    # worker_model.save(worker_model_path)
+    # eval_worker_env.close()
+    # env_worker.close()
     
     # Train Manager
     
@@ -113,7 +114,7 @@ def main():
     )
     
     eval_manager_env = build_env(eval_cfg, worker=False, worker_model=worker_model)
-    eval_manager_cb = EvalCallback(
+    eval_cb = EvalCallback(
         eval_manager_env,
         best_model_save_path=os.path.join(manager_cfg.logdir, "best_model"),
         log_path=os.path.join(manager_cfg.logdir, "eval"),
@@ -127,7 +128,7 @@ def main():
     # Learn
     manager_model.learn(
         total_timesteps=manager_cfg.total_timesteps,
-        callback=CallbackList([info_cb, eval_manager_cb, video_cb]),
+        callback=CallbackList([info_cb, eval_cb, video_cb]),
     )
 
     manager_model.save(os.path.join(manager_cfg.logdir, "model.pkl"))
