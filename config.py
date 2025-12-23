2414a99d48a9c48d899f27fb63cbc147c15d82b6
RTae <rtae@gmail.com>
2025-12-23 21:37:17 +0800

    update: change base image to CUDA 13.0.1 and adjust num_envs to 24

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     seed: int = getenv("SEED", 42, int)
 
     # IMPORTANT: keep this small early
-    num_envs: int = getenv("NUM_ENVS", 40, int)
+    num_envs: int = getenv("NUM_ENVS", 24, int)
 
     # Episode length (~3 seconds)
     episode_len: int = getenv("EPISODE_LEN", 300, int)
