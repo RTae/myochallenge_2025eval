a777e74067189578c78a64f51d189334c6bb15a5
rtae <rtae@gmail.com>
2025-12-27 23:39:16 +0800

    update: adjust number of environments to 38 in configuration

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     seed: int = getenv("SEED", 42, int)
 
     # IMPORTANT: keep this small early
-    num_envs: int = getenv("NUM_ENVS", 51, int)
+    num_envs: int = getenv("NUM_ENVS", 38, int)
 
     # Episode length (~3 seconds)
     episode_len: int = getenv("EPISODE_LEN", 300, int)
