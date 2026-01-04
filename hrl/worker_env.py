fix: adjust reward calculation in TableTennisWorker for improved alignment and reach distance

hrl/worker_env.py
@@ -378,8 +378,9 @@ class TableTennisWorker(CustomEnv):
 
         # Rewards
         reward = 0.0
-        reward += 5.0 * alignment_y
-        reward += 5.0 * alignment_z
+        reward += 10.0 * alignment_y
+        reward += 10.0 * alignment_z
+        reward += 2.0 * (1.0 - np.tanh(reach_dist))
         reward += 1.0 * paddle_quat_reward
         reward += 0.5 * pelvis_alignment
 